872331c1dc669d21b13602d659ae4c68c434202d
Shakhabutdinov Abdulaziz <shakhabutdinovabdulaziz@gmail.com>
2024-11-30 23:27:10 +0500

    Add project files.

---
+++ WAD.BACKEND.17145/Data/EventManagementDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAD.BACKEND._17145.Models;
+
+namespace WAD.BACKEND._17145.Data
+{
+    public class EventManagementDbContext:DbContext
+    {
+        public EventManagementDbContext(DbContextOptions<EventManagementDbContext> options) : base(options) { }
+
+        public DbSet<Events> Events { get; set; }
+        public DbSet<Participants> Participants { get; set; }
+    }
+}
